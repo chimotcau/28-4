da1c058773b55b7e5f8119a6bf6ff6471acdde0d
вонгуендангхоа <khoacenzo@gmail.com>
2023-04-27 17:27:59 +0300

    homework 28.4

---
+++ 1 tip 19.py
@@ -0,0 +1,12 @@
+def F( x,y,h):
+    if x +y <= 40 and h == 3:
+        return True
+    if x + y >40 and h ==3:
+        return False
+    return F(x-1,y,h+1) or F(x,y-1,h+1) or F(x//2,y,h+1) or F(x,y//2,h+1) 
+for s in range(100,20,-1):
+    if F(20,s,1) == True:
+        print(s)
+        break                
+
+            
